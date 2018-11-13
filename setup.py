cb2141953a7a4b47c7210229de21ba765d969252
heymoose <marcusanovak@gmail.com>
2018-11-13 14:19:03 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='intrinsify',
+      version='0.1',
+      description='Python package that pulls stock information and calculates a rough intrinsic value based on the Ben Graham formula',
+      url='https://github.com/heymoose/intrinsify',
+      author='Marc Novak',
+      license='MIT',
+      packages=[''],
+      zip_safe=False)
